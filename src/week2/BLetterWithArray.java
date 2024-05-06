32d31544bf13edd508f6a5df26b76fdf964141e2
Gonca Çomak <gonca.0045@hotmail.com>
2024-05-06 12:58:40 +0300

    print to screen B

---
+++ src/week2/BLetterWithArray.java
@@ -0,0 +1,35 @@
+package week2;
+
+// B letter with array
+
+public class BLetterWithArray {
+    public static void main(String[] args) {
+        String[][] letter = new String[7][4];
+
+        // add element for the array
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+
+                //    ---
+                //    ---
+                //    ---
+                if (i == 0 || i == 3 || i == 6) {
+                    letter[i][j] = " * ";
+                    // ->   |  |
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+        // print to screen " B "
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
+
