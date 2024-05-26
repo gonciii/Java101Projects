588632a1610c83df0e6ad120b28a595b88c95546
Gonca Çomak <gonca.0045@hotmail.com>
2024-05-26 22:47:12 +0300

    sql homework

--- src/postgreSql/week5/homework3.sql
+++ src/postgreSql/week5/homework3.sql
@@ -2,7 +2,7 @@
 --EXAMPLE 3
 ----------------------------------------------------------------------------
 
---SELECT country FROM country
+SELECT country FROM country
 WHERE country LIKE 'A%a';
 
 ------------------------------------------------------------------------------
@@ -11,10 +11,10 @@ WHERE country
 LIKE '%n' AND length(country) >= 6;
 
 ------------------------------------------------------------------------------
---SELECT title FROM film
+SELECT title FROM film
 WHERE title ~~* '%T%T%T%T%';
 
 ------------------------------------------------------------------------------
---SELECT * FROM film
+SELECT * FROM film
 WHERE title LIKE 'C%'
 AND length > 90  AND rental_rate = 2.99;
