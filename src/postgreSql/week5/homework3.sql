404bc9e7123a77ffa90f779b3374bcf0f4ccfdf9
Gonca Çomak <gonca.0045@hotmail.com>
2024-05-27 18:36:59 +0300

    sql homework

--- src/postgreSql/week5/homework3.sql
+++ src/postgreSql/week5/homework3.sql
@@ -1,20 +1,25 @@
 
 --EXAMPLE 3
 ----------------------------------------------------------------------------
-
+--country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
 WHERE country LIKE 'A%a';
 
 ------------------------------------------------------------------------------
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
 WHERE country
 LIKE '%n' AND length(country) >= 6;
 
 ------------------------------------------------------------------------------
+--film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
 SELECT title FROM film
 WHERE title ~~* '%T%T%T%T%';
 
 ------------------------------------------------------------------------------
+--film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
 SELECT * FROM film
 WHERE title LIKE 'C%'
 AND length > 90  AND rental_rate = 2.99;
