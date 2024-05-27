9306dd3cb02809c99226c60fe1749513585c6149
Gonca Çomak <gonca.0045@hotmail.com>
2024-05-27 19:33:53 +0300

    sql homework6

---
+++ src/postgreSql/week5/homework6.sql
@@ -0,0 +1,33 @@
+--EXAMPLE 6
+----------------------------------------------------------------------------------------
+--film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+
+SELECT ROUND(AVG(rental_rate),2)
+AS RENTAL_RATE_AVERAGE
+FROM film;
+
+
+---------------------------------------------------------------------------------------
+
+--film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+SELECT COUNT(*)
+FROM film
+WHERE title LIKE 'C%';
+
+
+-----------------------------------------------------------------------------------------
+--film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+
+SELECT MAX(length) MAX_MINUTE
+FROM film
+WHERE rental_rate = 0.99;
+
+
+
+--------------------------------------------------------------------------------------------
+
+--Film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+SELECT COUNT(DISTINCT(replacement_cost))
+FROM film
+WHERE length > 150;
